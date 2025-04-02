5175a10675bb04d296f06993e758ffa9ef78e352
unknown <you@example.com>
2025-04-02 15:05:06 -0700

    Task yarim hazirdir,yarisi ishlenecek

---
+++ 3AprilHomeTaskPolimorphism/ICodeAcademy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3AprilHomeTaskPolimorphism
+{
+    interface ICodeAcademy
+    {
+        public string CodeEmail { get; set; }
+
+        void GenerateMail(string name,string surname,int id);
+    }
+}
---
+++ 3AprilHomeTaskPolimorphism/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3AprilHomeTaskPolimorphism
+{
+    abstract class Student : ICodeAcademy
+    {
+        public static int Count = 0;
+        public int Id;
+        public string Name;
+        public string Surname;
+
+        protected Student()
+        {
+            Id = ++Count;
+        }
+
+        public string CodeEmail { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public static void CheckName(string name)
+        {
+
+        }
+
+
+        public void GenerateMail(string name, string surname, int id)
+        {
+            Name = name;
+            Surname = surname;
+            Id = id;
+            string combineWords = Name + "." + Surname;
+            Console.WriteLine(combineWords);
+        }
+    }
+}
